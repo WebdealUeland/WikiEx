ce7883ee8a65b6f8e0bd93d0cbbb946d32fef376
Tor Henning Ueland - Webdeal AS <tor.henning.ueland@webdealnetwork.com>
2009-08-07 16:32:10 +0200

    Fixing up src folder

--- src/init.rb
+++
@@ -1,8 +0,0 @@
-require 'redmine'
-
-Redmine::Plugin.register :redmine_wiki_ex do
-  name 'Redmine Wiki Ex(tended) plugin'
-  author 'Tor Henning Ueland - Webdeal AS'
-  description 'A complete Wiki system for Redmine'
-  version '0.0.1'
-end

51c2b510706b7d5d8a6543c34fd5938b06470364
Tor Henning Ueland - Webdeal AS <tor.henning.ueland@webdealnetwork.com>
2009-08-10 12:11:46 +0200

    The wiki now appears in project menu if wanted

--- src/init.rb
+++ src/init.rb
@@ -6,7 +6,16 @@ Redmine::Plugin.register :redmine_wikiex do
   description 'This is a extended wiki plugin'
   version '0.0.1'
 
-  menu :application_menu, :polls, { :controller => 'wikiex', :action => 'pages' }, :caption => 'Wiki Extended'
-  menu :project_menu, :polls, { :controller => 'wikiex', :action => 'pages' }, :caption => 'Wiki Extended'
-  menu :top_menu, :polls, { :controller => 'wikiex', :action => 'pages' }, :caption => 'Wiki Extended'
+  #menu :application_menu, :wikiex, { :controller => 'wikiex', :action => 'frontpage' }, :caption => 'Wiki Extended'
+  #permission :wikiex, {:wikiex => [:index, :edit]}, :public => true
+  #menu :project_menu, :wikiex, { :controller => 'wikiex', :action => 'pages' }, :caption => 'Wiki Extended', :after => :activity, :param => :project_id
+  menu :top_menu, :wikiex, { :controller => 'wikiex', :action => 'frontpage' }, :caption => 'Wiki Extended'
+
+  project_module :wikiex_module do
+    permission :view, {:wikiex => [:show, :project]}
+    permission :edit, {:wikiex => [:edit, :update, :new, :create, :destroy]}
+  end
+ 
+  menu :project_menu, :wikiex, {:controller => 'wikiex', :action => 'project'}, :caption => "Wiki Extended"
+
 end
